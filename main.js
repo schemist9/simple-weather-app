30617bf97263f242563943539dd16be6bdfcaeca
schemist9 <rubyenjoyer42@gmail.com>
2026-01-13 08:40:46 +0200

    Fetch days of the week info

--- main.js
+++ main.js
@@ -14,16 +14,60 @@ const getUserCoords = async city => {
     }
 }
 
-const getUserWeather = async (latitude, longitude) => {
-    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`)
+const getUserWeather = async (latitude, longitude, options="current_weather=true") => {
+    let URL = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}`;
+    if (options) {
+        URL += `&${options}`
+    }
+    const response = await fetch(URL)
     const data = await response.json();
     return data;
 }
 
+const getWeeklyWeather = async (latitude, longitude) => {
+    const result = await getUserWeather(latitude, longitude, 'daily=temperature_2m_min,temperature_2m_max');
+    return result    
+}
+const days = document.querySelector('.days-of-the-week').children;
+const renderDayOfTheWeek = (minTemp, maxTemp, dateISO, dayCount) => {
+    const dayElem = days[dayCount];
+    const date = (new Date(dateISO));
 
+    const day = date.getDate();
+    const dayElement = dayElem.querySelector('.day-number');    
+    dayElement.textContent = day;
+
+    const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    const weekday = weekdays[date.getDay()];
+    const weekdayElement = dayElem.querySelector('.day')
+    weekdayElement.textContent = weekday;
+
+    const months = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+    const month = months[date.getMonth()];
+    const monthElem = dayElem.querySelector('.month');
+    monthElem.textContent = month;
+
+    const minTempElement = dayElem.querySelector('.min-temp-count');
+    minTempElement.textContent = minTemp;
+
+
+    const maxTempElement = dayElem.querySelector('.max-temp-count');
+    maxTempElement.textContent = maxTemp;
+}
 
 (async () => {
     const { latitude, longitude } = await getUserCoords(await getUserCity());
-    console.log(await getUserWeather(latitude, longitude));
+    const { current_weather, current_weather_units } = await getUserWeather(latitude, longitude);
+    const res = await getWeeklyWeather(latitude, longitude);
+    for (let i = 0; i < 7; i++) {
+        const minTemp = res.daily.temperature_2m_min[i];
+        const maxTemp = res.daily.temperature_2m_max[i];
+        const date = res.daily.time[i];
+        renderDayOfTheWeek(minTemp, maxTemp, date, i)
+    }
+    
 })();
 
